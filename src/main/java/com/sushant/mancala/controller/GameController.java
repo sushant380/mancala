8f9d2027a58e4afbeae30073a7a5ff1f809d2136
sushant <sushant.patil@ikea.com>
2020-07-20 11:49:04 +0200

    Added doc security requirmemt

--- src/main/java/com/sushant/mancala/controller/GameController.java
+++ src/main/java/com/sushant/mancala/controller/GameController.java
@@ -7,7 +7,9 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.validation.constraints.Min;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -26,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/games")
 @Validated
+@SecurityRequirement(name="basicAuth")
 public class GameController {
 
   @Inject private GameService gameService;
